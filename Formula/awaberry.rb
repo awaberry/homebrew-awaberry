918e8f7b1868fd34ff502f8d34bb816c7ee20d4f
imagenotion <aw@imagenotion.com>
2026-03-19 10:07:30 +0100

    Add/Update awaberry formula

--- Formula/awaberry.rb
+++ Formula/awaberry.rb
@@ -16,60 +16,37 @@ class Awaberry < Formula
   def install
     bin.install "macbrewinstaller.sh" => "awaberry"
     chmod 0755, bin/"awaberry"
-  end
 
-# downlod install script
-# curl -s https://raw.githubusercontent.com/awaberry/awaberry/main/install.sh -o install.sh
-
-# brew services list - list of services
-# brew reinstall --build-from-source ./awaberry.rb
-# brew services start awaberry - start the service
-# brew services stop awaberry - stop the service
-
-
-service do
-  run [
-    "sh",
-    "-c",
-    "#{ENV["HOME"]}/awaberry/awaberryclient/update/update.sh && #{ENV["HOME"]}/awaberry/awaberryclient/app/runawaberryclient.sh"
-  ]
-  working_dir "#{ENV["HOME"]}/awaberry/awaberryclient/app"
-  log_path "#{ENV["HOME"]}/awaberry/awaberry.log"
-  error_log_path "#{ENV["HOME"]}/awaberry/error.log"
-end
-
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-        <dict>
-          <key>Label</key>
-          <string>com.awabon.awaberryclient</string>
-          <key>ProgramArguments</key>
-          <array>
-            <string>java</string>
-            <string>-Xmx64m</string>
-            <string>-cp</string>
-            <string>awaberryclient.jar:lib/*</string>
-            <string>com.awabon.client.mainapp.MainAppAwaberryClient</string>
-          </array>
-          <key>WorkingDirectory</key>
-          <string>#{ENV["HOME"]}/awaberry/awaberryclient/app</string>
-          <key>RunAtLoad</key>
-          <true/>
-          <key>StandardOutPath</key>
-          <string>#{ENV["HOME"]}/awaberryclient/.awaberrydata/execution.log</string>
-          <key>StandardErrorPath</key>
-          <string>#{ENV["HOME"]}/awaberry/execution.log</string>
-        </dict>
-      </plist>
+    # Create a named service launcher in libexec.
+    # macOS uses the binary basename as the background-item display name,
+    # so naming this script "awaberry" makes System Settings show "awaberry"
+    # instead of "sh".
+    launcher = libexec/"awaberry"
+    launcher.write <<~EOS
+      #!/bin/bash
+      "$HOME/awaberry/awaberryclient/update/update.sh" && \\
+      exec "$HOME/awaberry/awaberryclient/app/runawaberryclient.sh"
     EOS
+    chmod 0755, launcher
   end
 
+  # brew services list                   – list services
+  # brew services start awaberry         – start & register autostart on login/reboot
+  # brew services stop  awaberry         – stop & unregister autostart
+  # brew reinstall --build-from-source ./awaberry.rb
+
+  service do
+    # Run the named launcher so macOS reports "awaberry" in background items
+    run [opt_libexec/"awaberry"]
+    # keep_alive restarts the service if it exits and re-registers it after reboot
+    keep_alive true
+    working_dir "#{ENV["HOME"]}/awaberry/awaberryclient/app"
+    log_path     "#{ENV["HOME"]}/awaberry/awaberry.log"
+    error_log_path "#{ENV["HOME"]}/awaberry/error.log"
+  end
 
   test do
-    if File.exist?('>#{ENV["HOME"]}/awaberry/.awaberrydata/execution.log')
+    if File.exist?("#{ENV["HOME"]}/awaberry/.awaberrydata/execution.log")
       puts "Log file for awaberry exists - client is up."
     else
       puts "File does not exist."
